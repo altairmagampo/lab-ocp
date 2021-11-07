8172a2ce110dc4ee7f1c520623f90902cfb88a17
altair <lenrique.magallanes@gmail.com>
2021-11-06 21:20:55 -0600

    Test REST

---
+++ src/main/java/com/magallanes/altair/lab/ocp/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.magallanes.altair.lab.ocp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class TestController {
+	
+	@GetMapping
+	public ResponseEntity<String> test() {
+		return new ResponseEntity<String>("Success", HttpStatus.OK);
+	}
+	
+}
