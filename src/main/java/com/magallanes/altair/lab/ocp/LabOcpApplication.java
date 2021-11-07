064d5dace42f0c3999316266e37d65e1fae3b674
altair <lenrique.magallanes@gmail.com>
2021-11-06 20:35:29 -0600

    Inicial

---
+++ src/main/java/com/magallanes/altair/lab/ocp/LabOcpApplication.java
@@ -0,0 +1,13 @@
+package com.magallanes.altair.lab.ocp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabOcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabOcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magallanes/altair/lab/ocp/LabOcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magallanes.altair.lab.ocp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabOcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
